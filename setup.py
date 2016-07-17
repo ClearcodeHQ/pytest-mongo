992e30af8c9e3aa6748ccfd9487860a65f86928c
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2016-07-17 23:24:31 +0200

    fixed pylama errors

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-mongo. If not, see <http://www.gnu.org/licenses/>.
-
+"""Package definition for pytest-mongo."""
 
 import os
 import re
@@ -24,7 +24,9 @@ from setuptools import setup, find_packages
 
 here = os.path.dirname(__file__)
 with open(os.path.join(here, 'src', 'pytest_mongo', '__init__.py')) as v_file:
-    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+    package_version = re.compile(
+        r".*__version__ = '(.*?)'", re.S
+    ).match(v_file.read()).group(1)
 
 
 def read(fname):
@@ -71,7 +73,8 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
@@ -79,7 +82,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=requirements,
     tests_require=test_requires,
--- src/pytest_mongo/__init__.py
+++ src/pytest_mongo/__init__.py
@@ -16,7 +16,7 @@
 
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-mongo.  If not, see <http://www.gnu.org/licenses/>.
-
+"""pytest-mongo's main module."""
 
 import logging
 
