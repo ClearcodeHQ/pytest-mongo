6116749d40c6e679002609e427c8d53c8a3fd41b
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2018-11-16 09:57:11 +0100

    removed pylama from ci

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -1,8 +1,6 @@
 """pytest-mongo tests collection."""
 import os
 
-from pytest_mongo import factories
-
 
 def test_mongo(mongodb):
     """Simple test for mongodb connection to the set up process."""
@@ -10,18 +8,9 @@ def test_mongo(mongodb):
         "test1": "test1",
     }
 
-    db = mongodb['test_db']
-    db.test.insert_one(test_data)
-    assert db.test.find_one()['test1'] == 'test1'
-
-
-mongo_params = '--nojournal --noauth --nohttpinterface --noprealloc'
-
-mongo_proc2 = factories.mongo_proc(port=27070, params=mongo_params)
-mongodb2 = factories.mongodb('mongo_proc2')
-
-mongo_proc3 = factories.mongo_proc(port=27071, params=mongo_params)
-mongodb3 = factories.mongodb('mongo_proc3')
+    database = mongodb['test_db']
+    database.test.insert_one(test_data)
+    assert database.test.find_one()['test1'] == 'test1'
 
 
 def test_third_mongo(mongodb, mongodb2, mongodb3):
@@ -29,33 +18,29 @@ def test_third_mongo(mongodb, mongodb2, mongodb3):
     test_data_one = {
         "test1": "test1",
     }
-    db = mongodb['test_db']
-    db.test.insert_one(test_data_one)
-    assert db.test.find_one()['test1'] == 'test1'
+    database = mongodb['test_db']
+    database.test.insert_one(test_data_one)
+    assert database.test.find_one()['test1'] == 'test1'
 
     test_data_two = {
         "test2": "test2",
     }
-    db = mongodb2['test_db']
-    db.test.insert_one(test_data_two)
-    assert db.test.find_one()['test2'] == 'test2'
+    database = mongodb2['test_db']
+    database.test.insert_one(test_data_two)
+    assert database.test.find_one()['test2'] == 'test2'
 
     test_data_three = {
         "test3": "test3",
     }
-    db = mongodb3['test_db']
-    db.test.insert_one(test_data_three)
-    assert db.test.find_one()['test3'] == 'test3'
+    database = mongodb3['test_db']
+    database.test.insert_one(test_data_three)
+    assert database.test.find_one()['test3'] == 'test3'
 
 
 def test_mongo_proc(mongo_proc, mongo_proc2, mongo_proc3):
     """Several mongodb processes running."""
-    for m in (mongo_proc, mongo_proc2, mongo_proc3):
-        assert os.path.isfile('/tmp/mongo.{port}.log'.format(port=m.port))
-
-
-mongo_proc_rand = factories.mongo_proc(port=None, params=mongo_params)
-mongodb_rand = factories.mongodb('mongo_proc_rand')
+    for mongo in (mongo_proc, mongo_proc2, mongo_proc3):
+        assert os.path.isfile('/tmp/mongo.{port}.log'.format(port=mongo.port))
 
 
 def test_random_port(mongodb_rand):
