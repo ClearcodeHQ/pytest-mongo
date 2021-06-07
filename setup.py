fa732ef55706ce797bee46d60e10b94f54a4a01d
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2021-06-07 12:54:50 +0200

    Migrate package definition to cfg file

--- setup.py
+++ setup.py
@@ -17,64 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-mongo. If not, see <http://www.gnu.org/licenses/>.
 """Package definition for pytest-mongo."""
-
-import os
-from setuptools import setup, find_packages
-
-here = os.path.dirname(__file__)
+from setuptools import setup
 
 
-def read(fname):
-    """
-    Read given file's content.
-
-    :param str fname: file name
-    :returns: file contents
-    :rtype: str
-    """
-    return open(os.path.join(here, fname)).read()
-
-
-requirements = ["pytest>=3.0.0", "pymongo>=3.6", "mirakuru", "port-for"]
-
-test_requires = ["pytest", "pytest-cov", "pytest-xdist"]
-
-extras_require = {"docs": ["sphinx"], "tests": test_requires}
-
-setup(
-    name="pytest-mongo",
-    version="2.1.0",
-    description="MongoDB process and client fixtures plugin for py.test.",
-    long_description=(read("README.rst") + "\n\n" + read("CHANGES.rst")),
-    keywords="tests py.test pytest fixture mongo mongodb",
-    author="Clearcode - The A Room",
-    author_email="thearoom@clearcode.cc",
-    url="https://github.com/ClearcodeHQ/pytest-mongo",
-    license="LGPL",
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: "
-        "GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    install_requires=requirements,
-    tests_require=test_requires,
-    test_suite="tests",
-    include_package_data=True,
-    zip_safe=False,
-    extras_require=extras_require,
-    entry_points={"pytest11": ["pytest_mongo = pytest_mongo.plugin"]},
-)
+setup()
